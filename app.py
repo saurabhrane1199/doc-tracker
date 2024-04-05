46d847cb59b45ab71388101b2e005aa4dcd318b4
saurabhrane1199 <saurabhrane1199@gmail.com>
2024-04-05 00:50:26 -0400

    Initial Working Setup

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, send_file
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import storage
+import io
 
 
 app = Flask(__name__)
 app.secret_key = "secret"
 cred = credentials.Certificate("secret.json")
-firebase_admin.initialize_app(cred)
+default_app = firebase_admin.initialize_app(cred, {'storageBucket': "doc-tracker-4791c.appspot.com"})
+bucket = storage.bucket()
+
+def test_storage():
+    blob = bucket.blob('test.gif').download_as_string()
+    return blob
 
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return jsonify({'message': 'Invalid password'}), 401
+    userAgent = request.headers['User-Agent']
+    remoteAddr =  request.remote_addr
+    print(f"{userAgent},  {remoteAddr}")
+    blob = test_storage()
+    return send_file(io.BytesIO(blob), mimetype='image/gif')
 
 
 
 
 if __name__ == '__main__':
     app.debug=True
-    default_app = firebase_admin.initialize_app()
     app.run(host='127.0.0.1', port=5000)
